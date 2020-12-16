6956f1c1b358a26454fa6fb8f3b4ce83d1aff3a2
Yurii Nemirich <nemirichyuri@gmail.com>
2020-12-16 19:45:01 +0200

    adapt start

--- src/js/script.js
+++ src/js/script.js
@@ -244,7 +244,22 @@ $(document).ready(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prev_product.png" alt="prev"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next_product.png" alt="next"></button>'
    });
+
+   $('.leaf__slider').slick({
+   dots: false,
+   infinite: true,
+   speed: 500,
+   fade: true,
+   cssEase: 'linear',
+   prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prev_product.png" alt="prev"></button>',
+   nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next_product.png" alt="next"></button>'
+   });
 });
+
+//    СЛАЙДЕР НОВОСТЕЙ
+
+
+
 //    ПЛАВНАЯ ПРОКРУТКА И КНОПКА ВВЕРХ
 
 $(window).scroll(function () {
@@ -255,6 +270,25 @@ $(window).scroll(function () {
    }
 });
 
+//    ГАМБУРГЕР 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
 
 //    ВАЛИДАЦИЯ ФОРМ
 
